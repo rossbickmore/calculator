76c3a31dcea3a1221e1d4b005691cba98f64119c
Ross Bickmore <rossbickmore@gmail.com>
2019-09-16 16:04:59 +0100

    added CSS to App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const AppStyle = {
+  textAlign: 'center',
+  backgroundColor: '#282c34',
+  height: '75vmin',
+  margin: '20% 20%',
+  display: 'flex',
+  flexDirection: 'row'
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={AppStyle}>
     </div>
   );
 }
